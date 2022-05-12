e1e586c30626e51ea3ee326ba9926e8bac2b2ded
goohayami <98527002+goohayami@users.noreply.github.com>
2022-05-12 23:53:34 +0900

    5_12 change goalNum

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 function question() {
+  const min0 = 6;
+  const max0 = 10;
+  const origin = Math.floor(Math.random() * (max0 + 1 - min0) + min0);
+  const originNum = origin;
+  const finalGoal = document.getElementById("goalNum");
+  finalGoal.innerText = originNum;
+  console.log(originNum);
   const min = 1;
-  const max = 9;
+  const max = originNum - 1;
   const left = Math.floor(Math.random() * (max + 1 - min) + min);
-  console.log(left);
   const leftnum = document.getElementById("leftNum");
   leftnum.innerText = left;
-  console.log(left);
+
   const right = document.getElementById("rightNum");
-  const anser = 10 - left;
+  const anser = originNum - left;
   right.innerText = anser;
+  const tellMe = document.getElementById("tellMe");
+  tellMe.innerText = `${originNum}は${left}と${anser}`;
 }
 
 const btn = document.getElementById("sayAnser");
 btn.addEventListener("click", () => {
   document.querySelector("#rightNum").classList.add("newRight");
+  document.querySelector("#tellMe").classList.add("tellMeChange");
 });
 
 question();
 const btnNext = document.getElementById("newQuestion");
 btnNext.addEventListener("click", () => {
   const nowQ = document.getElementById("rightNum");
+  const tellAll = document.getElementById("tellMe");
   nowQ.classList.remove("newRight");
-  console.log(nowQ);
+  tellAll.classList.remove("tellMeChange");
   question();
 });
